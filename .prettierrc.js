1912a7705bc6f17c133097dc27201bbf53a0921c
yunanah <bbyl6319@gmail.com>
2022-06-14 00:23:11 +0900

    🎨 [TA_C] eslint, prettier 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  bracketSameLine: true, // JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+  singleQuote: true, // singe 쿼테이션 사용 여부
+  trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+  printWidth: 100, //  줄 바꿈 할 폭 길이
 };
